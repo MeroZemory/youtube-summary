974eeebee0e62f14ca8ebebc447b01ef4319e7fe
MeroZemory <merozemory@gmail.com>
2025-02-05 01:11:28 +0900

    fix: button 중첩 구조 수정
    - HTML 유효성 검사 오류 해결: button 안에 button이 중첩된 구조 제거
    - 바깥쪽 button을 div로 변경하고 role="tab" 추가하여 접근성 유지
    - 탭 선택 및 이름 변경 기능은 그대로 유지

---
+++ src/app/components/AnalysisTabs.tsx
@@ -0,0 +1,9 @@
+<div className="AnalysisTabs_tab__u1GoM AnalysisTabs_selected__KO2ZG" role="tab">
+  <div className="AnalysisTabs_tabContent">
+    <div className="AnalysisTabs_tabActions">
+      <button className="AnalysisTabs_renameButton">
+        ...
+      </button>
+    </div>
+  </div>
+</div> 
